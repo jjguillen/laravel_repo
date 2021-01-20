820a63ea7b36f656debb3060971d4841a09c2dc0
Javier GB <jjavierguillen@gmail.com>
2021-01-20 18:58:49 +0100

    Blade templates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,19 +65,12 @@
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="position-sticky pt-3">
         <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link @yield('active1')" aria-current="page" href="incidencias">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-              Incidencias
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link @yield('active2')" href="empleados">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
-              Empleados
-            </a>
-          </li>
-          
+          <x-menu_li>
+            Incidencias
+          </x-menu_li>
+          <x-menu_li>
+            Empleados
+          </x-menu_li>
         </ul>
       </div>
     </nav>
